367c5f6c82baeec5059e154c4fa1055f1f93a378
MaciejUlasiuk <filip123xd@o2.pl>
2023-02-18 20:53:23 +0100

    Loader add

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,13 @@
+import "./loader.css";
+const Loader = () => {
+  return (
+    <>
+      <div className="loader-box">
+        <h1 className="loading">Loading Content</h1>
+        <div className="loader"></div>
+      </div>
+    </>
+  );
+};
+
+export default Loader;
