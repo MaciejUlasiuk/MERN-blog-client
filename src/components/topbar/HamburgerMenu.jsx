d1393315a2214b2a1919b519fc16e982b7b0c5cf
MaciejUlasiuk <filip123xd@o2.pl>
2023-02-19 14:35:49 +0100

    route

--- src/components/topbar/HamburgerMenu.jsx
+++ src/components/topbar/HamburgerMenu.jsx
@@ -18,7 +18,7 @@ const HamburgerMenu = () => {
             </Link>
           </li>
           <li className="topListItemm">
-            <Link className="link" to="/about">
+            <Link className="link" to="/ax">
               ABOUT
             </Link>
           </li>
